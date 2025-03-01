d3fbe6a0c44169d9347ff5713f87b6ad1a77a94e
cvanelteren <caspervanelteren@gmail.com>
2025-03-01 18:46:04 +0100

    fix issue where cat. data is now shown correctly

--- ultraplot/internals/inputs.py
+++ ultraplot/internals/inputs.py
@@ -678,7 +678,7 @@ def _meta_labels(data, axis=0, always=True):
     # NOTE: Even if coords not present .coords[dim] auto-generates indices
     elif isinstance(data, DataArray):
         if axis < data.ndim:
-            labels = data.coords[data.dims[axis]]
+            labels = np.asarray(data.coords[data.dims[axis]])
         elif not always:
             pass
         else:
