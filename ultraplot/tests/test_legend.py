a43a5c44beea53486f7abd82cdf67d88d18120f2
cvanelteren <caspervanelteren@gmail.com>
2025-03-15 07:54:53 +0100

    add input testing

--- ultraplot/tests/test_legend.py
+++ ultraplot/tests/test_legend.py
@@ -191,7 +191,10 @@ def test_legend_col_spacing():
     ax.plot(state.rand(10), label="longer label")
     ax.plot(state.rand(10), label="even longer label")
     for idx in range(3):
-        ax.legend(loc="bottom", ncol=3, columnspacing=f"{idx}em")
+        spacing = f"{idx}em"
+        if idx == 2:
+            spacing = 3
+        ax.legend(loc="bottom", ncol=3, columnspacing=spacing)
 
     with pytest.raises(ValueError):
         ax.legend(loc="bottom", ncol=3, columnspacing="15x")
