19be641a63bbd1d339d3226060c1047a2a521ab8
cvanelteren <caspervanelteren@gmail.com>
2025-04-09 13:57:12 +0200

    add motivation

--- ultraplot/tests/test_format.py
+++ ultraplot/tests/test_format.py
@@ -396,6 +396,7 @@ def test_input_parsing_cycle():
 
 
 def test_scaler():
+    # Test a ultraplot scaler and a matplotlib native scaler; should not race errors
     fig, ax = uplt.subplots(ncols=2, share=0)
     ax[0].set_yscale("mercator")
     ax[1].set_yscale("asinh")
