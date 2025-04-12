2c00a849d8bf840d3fb8d06553816407b089c402
cvanelteren <caspervanelteren@gmail.com>
2025-04-12 17:24:11 +0200

    add label param test

--- ultraplot/tests/test_plot.py
+++ ultraplot/tests/test_plot.py
@@ -3,26 +3,29 @@ from unittest import mock
 
 
 @pytest.mark.parametrize(
-    "mpl_version, expected_key_and_value",
+    "mpl_version, expected_key, expected_value",
     [
-        ("3.10.0", dict(orientation="vertical", ticklabels=["hello", "world", "!"])),
-        ("3.9.0", dict(vert=True, labels=["hello", "world", "!"])),
+        ("3.10.0", "orientation", "vertical"),
+        ("3.9.0", "vert", True),
     ],
 )
 def test_violinplot_versions(
     mpl_version: str,
-    expected_key_and_value: dict,
+    expected_key: str,
+    expected_value: bool | str,
 ):
     fig, ax = uplt.subplots()
     with mock.patch("ultraplot.axes.plot._version_mpl", new=mpl_version):
         with mock.patch.object(ax.axes, "_call_native") as mock_call:
-            ax.violinplot(y=[1, 2, 3], vert=True, labels=["hello", "world", "!"])
+            # Note: implicit testing of labels passing. It should work
+            ax.violinplot(y=[1, 2, 3], vert=True, labels=["hello world!"])
+            xtick_labels = ax.get_xticklabels()
+            assert len(xtick_labels) == 1
+            assert xtick_labels[0].get_text() == "hello world!"
 
             mock_call.assert_called_once()
             _, kwargs = mock_call.call_args
-            print(_)
-            for expected_key, expected_value in expected_key_and_value.items():
-                assert kwargs[expected_key] == expected_value
+            assert kwargs[expected_key] == expected_value
             if expected_key == "orientation":
                 assert "vert" not in kwargs
             else:
