4119f8b363bf17865db1faadd83ce1665a92377b
cvanelteren <caspervanelteren@gmail.com>
2025-04-12 17:40:59 +0200

    separate tests

--- ultraplot/tests/test_plot.py
+++ ultraplot/tests/test_plot.py
@@ -9,6 +9,25 @@ from unittest import mock
         ("3.9.0", "vert", True),
     ],
 )
+def test_violin_labels():
+    """
+    Test the labels functionality of violinplot and violinploth.
+    """
+    fig, ax = uplt.subplots()
+    ax.violinplot(y=[1, 2, 3], vert=True, labels=["hello world!"])
+    xtick_labels = ax.get_xticklabels()
+    assert len(xtick_labels) == 1
+    assert xtick_labels[0].get_text() == "hello world!"
+
+    ax.violinploth(y=[1, 2, 3], vert=True, labels=["hello world!"])
+
+    # Also test the horizontal ticks
+    ax.violinploth(x=[1, 2, 3], labels=["hello world!"])
+    ytick_labels = ax.get_yticklabels()
+    assert len(ytick_labels) == 1
+    assert ytick_labels[0].get_text() == "hello world!"
+
+
 def test_violinplot_versions(
     mpl_version: str,
     expected_key: str,
@@ -21,10 +40,7 @@ def test_violinplot_versions(
     with mock.patch("ultraplot.axes.plot._version_mpl", new=mpl_version):
         with mock.patch.object(ax.axes, "_call_native") as mock_call:
             # Note: implicit testing of labels passing. It should work
-            ax.violinplot(y=[1, 2, 3], vert=True, labels=["hello world!"])
-            xtick_labels = ax.get_xticklabels()
-            assert len(xtick_labels) == 1
-            assert xtick_labels[0].get_text() == "hello world!"
+            ax.violinplot(y=[1, 2, 3], vert=True)
 
             mock_call.assert_called_once()
             _, kwargs = mock_call.call_args
@@ -33,12 +49,6 @@ def test_violinplot_versions(
                 assert "vert" not in kwargs
             else:
                 assert "orientation" not in kwargs
-
-            # Also test the horizontal ticks
-            ax.violinploth(x=[1, 2, 3], labels=["hello world!"])
-            ytick_labels = ax.get_yticklabels()
-            assert len(ytick_labels) == 1
-            assert ytick_labels[0].get_text() == "hello world!"
 
 
 def test_hatches():
