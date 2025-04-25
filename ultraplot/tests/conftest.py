b7f080310f28cbbbd0974925e1cdc91653c9aeea
cvanelteren <caspervanelteren@gmail.com>
2025-04-25 17:33:43 +0200

    rm captures

--- ultraplot/tests/conftest.py
+++ ultraplot/tests/conftest.py
@@ -2,8 +2,6 @@ import os, shutil, pytest, re, numpy as np, ultraplot as uplt
 from pathlib import Path
 import warnings, logging
 
-logging.captureWarnings(True)
-
 
 @pytest.fixture(autouse=True)
 def _reset_numpy_seed():
@@ -76,8 +74,6 @@ class StoreFailedMplPlugin:
 def pytest_configure(config):
     print("Configuring StoreFailedMplPlugin")
     # Surpress ultraplot config loading which mpl does not recognize
-    logging.getLogger("matplotlib").setLevel(logging.ERROR)
-    logging.getLogger("ultraplot").setLevel(logging.WARNING)
     if rc_file := config.getoption("--mpl-default-style", None):
         warnings.filterwarnings(
             "ignore",
