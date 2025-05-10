5a817fa2895fd5506e987dccad1509f0f88caaa2
cvanelteren <caspervanelteren@gmail.com>
2025-05-10 18:29:28 +0200

    add update extent after setting the ticks

--- ultraplot/axes/geo.py
+++ ultraplot/axes/geo.py
@@ -1050,19 +1050,6 @@ class GeoAxes(shared._SharedAxes, plot.PlotAxes):
                 self._lonaxis.get_major_locator()._dms = dms
                 self._lataxis.get_major_locator()._dms = dms
 
-            # Set tick lengths for flat projections
-            lonticklen = _not_none(lonticklen, ticklen)
-            latticklen = _not_none(latticklen, ticklen)
-            if lonticklen or latticklen:
-                # Only add warning when ticks are given
-                if _is_rectilinear_projection(self):
-                    self._add_geoticks("x", lonticklen, ticklen)
-                    self._add_geoticks("y", latticklen, ticklen)
-                else:
-                    warnings._warn_ultraplot(
-                        f"Projection is not rectilinear. Ignoring {lonticklen=} and {latticklen=} settings."
-                    )
-
             # Apply worker extent, feature, and gridline functions
             lonlim = _not_none(lonlim, default=(None, None))
             latlim = _not_none(latlim, default=(None, None))
@@ -1084,6 +1071,21 @@ class GeoAxes(shared._SharedAxes, plot.PlotAxes):
                 latgrid=latgridminor,
                 nsteps=nsteps,
             )
+        # Set tick lengths for flat projections
+        lonticklen = _not_none(lonticklen, ticklen)
+        latticklen = _not_none(latticklen, ticklen)
+        if lonticklen or latticklen:
+            # Only add warning when ticks are given
+            if _is_rectilinear_projection(self):
+                self._add_geoticks("x", lonticklen, ticklen)
+                self._add_geoticks("y", latticklen, ticklen)
+                self._update_extent(
+                    lonlim=lonlim, latlim=latlim, boundinglat=boundinglat
+                )
+            else:
+                warnings._warn_ultraplot(
+                    f"Projection is not rectilinear. Ignoring {lonticklen=} and {latticklen=} settings."
+                )
 
         # Parent format method
         super().format(rc_kw=rc_kw, rc_mode=rc_mode, **kwargs)
