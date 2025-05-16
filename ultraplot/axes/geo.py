95bc515bcd18aa3cb13e9c1e32e3668b046eaf96
cvanelteren <caspervanelteren@gmail.com>
2025-05-16 20:41:45 +0200

    confirm fix and is caused by colorbar

--- ultraplot/axes/geo.py
+++ ultraplot/axes/geo.py
@@ -748,7 +748,8 @@ class GeoAxes(shared._SharedAxes, plot.PlotAxes):
         for axi in self.figure.axes:
             # If users use PlotAxes methods, we need not
             # toggle the gridliner labels; we can just skip.
-            print(type(axi))
+            if not isinstance(axi, GeoAxes):
+                continue
 
             sides = recoded.get(axi, [])
             tmp = default.copy()
