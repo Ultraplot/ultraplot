7715b1ee45c57788c0c3801a32303596e8e74dcd
cvanelteren <caspervanelteren@gmail.com>
2025-05-27 19:35:08 +0200

    rm debug

--- ultraplot/figure.py
+++ ultraplot/figure.py
@@ -955,7 +955,6 @@ class Figure(mfigure.Figure):
             elif y > grid.shape[1] - 1:
                 return True
             # Check if we reached a plot or an internal edge
-            print(grid.shape, x, y)
             if grid[x, y] != target and grid[x, y] > 0:
                 return False
             elif grid[x, y] != target and grid[x, y] == 0:
