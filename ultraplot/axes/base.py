c2f9afeb9e60da151eb9aba6b058a8c4d433ebb5
cvanelteren <caspervanelteren@gmail.com>
2025-06-07 09:44:02 +0200

    revert change

--- ultraplot/axes/base.py
+++ ultraplot/axes/base.py
@@ -1695,6 +1695,9 @@ class Axes(maxes.Axes):
             indicator = self._inset_zoom_artists
             indicator.rectangle.update(kwargs)
             indicator.rectangle.set_bounds(bounds)  # otherwise the patch is not updated
+            indicator.rectangle.set_zorder(
+                self.get_zorder() + 1
+            )  # Ensure rectangle appears above axes
             z = self.get_zorder() + 1
             indicator.rectangle.set_zorder(z)
             for connector in indicator.connectors:
