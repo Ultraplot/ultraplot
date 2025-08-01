7bb883e0fc8edad1fa96a8d09dcfd510840a07f8
cvanelteren <caspervanelteren@gmail.com>
2025-08-01 16:23:33 +0200

    minor refactor

--- ultraplot/legend.py
+++ ultraplot/legend.py
@@ -19,14 +19,14 @@ class Legend(mlegend.Legend):
     @override
     def set_loc(self, loc=None):
         # Sync location setting with the move
+        old_loc = None
         if self.axes is not None:
             # Get old location which is a tuple of location and
             # legend type
-            old_loc = None
             for k, v in self.axes._legend_dict.items():
                 old_loc = k
             value = self.axes._legend_dict.pop(old_loc, None)
-            super().set_loc(loc)
-            if old_loc:
-                where, type = old_loc
-                self.axes._legend_dict[(loc, type)] = value
+        super().set_loc(loc)
+        if old_loc:
+            where, type = old_loc
+            self.axes._legend_dict[(loc, type)] = value
