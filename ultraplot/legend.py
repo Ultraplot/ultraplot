320298133ec5ecb8b61c1bf9ac8082fc2ca70c21
cvanelteren <caspervanelteren@gmail.com>
2025-08-01 16:10:07 +0200

    wrap matplotlib legend location setting to sync with legend_dict`

---
+++ ultraplot/legend.py
@@ -0,0 +1,32 @@
+from typing_extensions import override
+from matplotlib import legend as mlegend
+
+try:
+    from typing import override
+except:
+    from typing_extensions import override
+
+
+class Legend(mlegend.Legend):
+    # Soft wrapper of matplotlib legend's class.
+    # Currently we only override the syncing of the location.
+    # The user may change the location and the legend_dict should
+    # be updated accordingly. This caused an issue where
+    # a legend format was not behvaving according to the docs
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @override
+    def set_loc(self, loc=None):
+        # Sync location setting with the move
+        if self.axes is not None:
+            # Get old location which is a tuple of location and
+            # legend type
+            old_loc = None
+            for k, v in self.axes._legend_dict.items():
+                old_loc = k
+            value = self.axes._legend_dict.pop(old_loc, None)
+            super().set_loc(loc)
+            if old_loc:
+                where, type = old_loc
+                self.axes._legend_dict[(loc, type)] = value
